f43635275d629acb404ef8863312f9d2937c254b
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-03-09 16:26:33 -0800

    Upload T-Tests on Suspension Coils

--- MechaCarAnalysis.R
+++ MechaCarAnalysis.R
@@ -9,3 +9,13 @@ suspension_coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsA
 total_summary<-suspension_coil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
 lot_summary<-suspension_coil_table%>% group_by(Manufacturing_Lot)%>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
                            
+t.test(suspension_coil_table$PSI,mu=1500)
+
+Lot1<-subset(suspension_coil_table,Manufacturing_Lot=="Lot1")
+Lot2<-subset(suspension_coil_table,Manufacturing_Lot=="Lot2")
+Lot3<-subset(suspension_coil_table,Manufacturing_Lot=="Lot3")
+
+t.test(Lot1$PSI,mu=1500)
+t.test(Lot2$PSI,mu=1500)
+t.test(Lot3$PSI,mu=1500)
+
