979ff5ecd5d6cbeb214f3e609b56f7178b07373f
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-03-09 16:17:17 -0800

    Upload Summary Statistics on Suspension Coils

--- MechaCarAnalysis.R
+++ MechaCarAnalysis.R
@@ -3,3 +3,9 @@ library(dplyr)
 mechacar_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechacar_table)
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechacar_table))
+
+
+suspension_coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+total_summary<-suspension_coil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
+lot_summary<-suspension_coil_table%>% group_by(Manufacturing_Lot)%>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI),Standard_deviation_PSI=sd(PSI))
+                           
